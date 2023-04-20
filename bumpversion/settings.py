2d54f94f6626cc2a32b83cdd3e9d4f41a4502bd7
Tomáš Pazderka <tomas.pazderka@nic.cz>
2023-04-20 16:33:29 +0200

    Update settings and add validation

--- bumpversion/settings.py
+++ bumpversion/settings.py
@@ -3,7 +3,7 @@ import os
 from typing import Any, Dict, List, Optional, Tuple, cast
 
 import tomli
-from pydantic import BaseModel, BaseSettings, Extra, Field, FilePath, PrivateAttr
+from pydantic import BaseModel, BaseSettings, Extra, Field, FilePath, PrivateAttr, root_validator
 from pydantic.env_settings import SettingsSourceCallable
 
 from .constants import Verbosity
@@ -44,20 +44,12 @@ class Component(BaseModel):
         extra = Extra.allow
 
 
-class Schema(BaseModel):
-    """Definition of versioning schema."""
-
-    parser: Component
-    bumper: Component
-    serializer: Component
-    replacer: Component
-
-
 class File(BaseModel):
     """Definition of maintained file."""
 
     path: FilePath
-    version_schema: Optional[str] = Field(default=None, alias="schema")
+    serializer: Optional[Component] = None
+    replacer: Optional[Component] = None
 
     class Config:
         extra = Extra.allow
@@ -74,7 +66,11 @@ class Settings(BaseSettings):
     tag: bool = False
     allow_dirty: bool = False
     current_version: Optional[str] = None
-    version_schema: Dict[str, Schema] = Field(default_factory=dict, alias="schema")
+    version_schema: Optional[str] = Field(default=None, alias="schema")
+    bumper: Optional[Component] = None
+    parser: Optional[Component] = None
+    serializer: Optional[Component] = None
+    replacer: Optional[Component] = None
     file: List[File] = []
 
     class Config:
@@ -98,3 +94,32 @@ class Settings(BaseSettings):
     def __init__(self, *args: Any, config_file: Optional[str] = None, **kwargs: Any):
         self._config_file = config_file
         super().__init__(*args, **kwargs)
+
+    @root_validator
+    def schema_definiton(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+        """Check that we either have a schema or all necessary parts."""
+        if values.get("version_schema") is not None:
+            # Schema is defined, we are going to use that
+            return values
+        bumper = values.get("bumper")
+        parser = values.get("parser")
+        serializer = values.get("serializer")
+        replacer = values.get("replacer")
+        if (
+            bumper is not None
+            and parser is not None
+            and serializer is not None
+            and replacer is not None
+        ):
+            # We have all parts defined separatelly
+            return values
+        if bumper is not None and parser is not None:
+            # We have root level parser and bumper...
+            # Lets have a look if we have parser/serializer in files
+            for file in values.get("file", []):
+                if file.serializer is None and serializer is None:
+                    raise ValueError(f"{file.path} setting is missing serializer option")
+                if file.replacer is None and replacer is None:
+                    raise ValueError(f"{file.path} setting is missing replacer option")
+            return values
+        raise ValueError("Incomplete schema definition settings")
