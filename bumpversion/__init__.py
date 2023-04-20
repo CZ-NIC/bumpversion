14d027fa9e437653b01782b3a3d70f05ca03031b
Jan Musílek <jan.musilek@nic.cz>
2023-04-20 11:35:05 +0200

    Fix #4 - Add parsers and serializers

--- bumpversion/__init__.py
+++ bumpversion/__init__.py
@@ -1,3 +1,12 @@
 """Bumpversion app."""
+from .parser import PEP440Parser, SemVerParser
+from .serializer import PEP440Serializer, SemVerSerializer
 
 __version__ = "0.1.0"
+
+__all__ = [
+    "PEP440Parser",
+    "PEP440Serializer",
+    "SemVerParser",
+    "SemVerSerializer",
+]
